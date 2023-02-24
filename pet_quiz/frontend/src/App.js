a78131cdc5058fe57f822ef025eb25596a5e4ca7
shunsuke-kawata <kwt20238120@gmail.com>
2023-02-25 02:15:50 +0900

    delete && rename 不要なファイルの削除とフォルダのリネーム

---
+++ pet_quiz/frontend/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+          Learn React
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
