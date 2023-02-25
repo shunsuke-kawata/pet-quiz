1e4d5d0e6d2bc1165ba8ae0c530e27248d8a60fd
shunsuke kawata <kwt20238120@gmail.com>
2023-02-25 16:20:54 +0900

    clone && update macでの作業開始とjsの変更

--- pet_quiz/frontend/src/App.js
+++ pet_quiz/frontend/src/App.js
@@ -1,14 +1,10 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-          Learn React
-       
+        <h1>ガバイソンチーム２</h1>
       </header>
     </div>
   );
