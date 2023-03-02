170856f66b0391d49e490a9fe619e05e935a2fc0
smaru1111 <marusou20@gmail.com>
2023-03-03 01:49:52 +0900

    #16 update firebaseからデータ取得

--- pet_quiz/frontend/src/Pages/Question.js
+++ pet_quiz/frontend/src/Pages/Question.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import "../css/Question.css";
 
-export default function Question() {
-  const member = ["Araki", "Ibata", "Fukutome", "Woods"];
 
-  const optionObj = member.map((item, idx) =>
+const Question = () => {
+
+  // reactセクション
+  const choices = ["a","a","a","a"]
+
+  const optionObj = choices.map((item, idx) =>
     <a href="result">
         <button className='option-btn' key={idx}>
           {idx+1}. {item}
         </button>
     </a>
-  // Only do this if items have no stable IDs
   // <Option key={item} toLink="result" optionContent={item} contentId={idx}/>
-);
-console.log(optionObj);
+  );
   return (
     <div className='question-container'>
       <h1>問題文問題文問題文</h1>
@@ -23,3 +24,4 @@ console.log(optionObj);
     </div>
   )
 }
+export default Question;
