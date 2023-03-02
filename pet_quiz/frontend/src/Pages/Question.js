f5b45c98e81e9dd123c6aeff75d8cb17b3263fc6
shunsuke kawata <kwt20238120@gmail.com>
2023-03-03 02:21:40 +0900

    update axiosのバージョン変更

--- pet_quiz/frontend/src/Pages/Question.js
+++ pet_quiz/frontend/src/Pages/Question.js
@@ -1,48 +1,38 @@
-import React from 'react';
+import React from "react";
 import "../css/Question.css";
-import { useState } from 'react';
-import TotalResult from './TotalResult';
+import { useState } from "react";
+import TotalResult from "./TotalResult";
 
-
-export default function Question({setanswerFlag,setqandaFlag,questionFlag,rightCount}) {
+export default function Question({
+  setanswerFlag,
+  setqandaFlag,
+  questionFlag,
+  rightCount,
+}) {
   const member = ["Araki", "Ibata", "Fukutome", "Woods"];
   //問題の正誤を保持する変数
-  
 
-  const handleClick=(flag)=>{
-    setqandaFlag(true)
-    setanswerFlag(flag) //問題からとってきた正誤の変数
-  }
+  const handleClick = (flag) => {
+    setqandaFlag(true);
+    setanswerFlag(flag); //問題からとってきた正誤の変数
+  };
 
-  const optionObj = member.map((item, idx) =>
-        <button className='option-btn' key={idx} onClick={() => handleClick(true)}>
-          {idx+1}. {item}
-        </button>
-   /* <a href="result">
-        <button className='option-btn' key={idx} onClick={() => setqandaFlag(true)}>
-          {idx+1}. {item}
-        </button>
-    </a>*/
-  // Only do this if items have no stable IDs
-  // <Option key={item} toLink="result" optionContent={item} contentId={idx}/>
-);
-console.log(optionObj);
-console.log(rightCount);
+  const optionObj = member.map((item, idx) => {
+    <button className="option-btn" key={idx} onClick={() => handleClick(true)}>
+      {idx + 1}. {item}
+    </button>;
+  });
+
   return (
     <>
-    {questionFlag ?(<TotalResult rightCount={rightCount}/>):(<div className='question-container'>
-      <h1>問題文問題文問題文</h1>
-      <div className='options'>
-        {optionObj}
-      </div>
-    </div>)}
-      </>
-    /*<div className='question-container'>
-      <h1>問題文問題文問題文</h1>
-      <div className='options'>
-        {optionObj}
-      </div>
-    </div>*/
-  )
+      {questionFlag ? (
+        <TotalResult rightCount={rightCount} />
+      ) : (
+        <div className="question-container">
+          <h1>問題文問題文問題文</h1>
+          <div className="options">{optionObj}</div>
+        </div>
+      )}
+    </>
+  );
 }
-export default Question;
