7b9e6542dc4b4939c3f9aeedef7d4cadabc1f8e2
keyboye <Muramasa126111@outlook.com>
2023-03-03 01:19:37 +0900

    update 画面遷移を実装

--- pet_quiz/frontend/src/Pages/TotalResult.js
+++ pet_quiz/frontend/src/Pages/TotalResult.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Button from '../Components/Button'
 import '../css/TotalResult.css'
 
-export default function TotalResult() {
+export default function TotalResult({rightCount}) {
+  console.log(rightCount)
   return (
     
     <div>
       <div className='title'>結果発表！</div>
-      <div className='result'>n/10問正解</div>
+      <div className='result'>{rightCount}/10問正解</div>
       <p>なんか適当な文</p>
       <div className='bon'>
       <Button toLink="/" ButtonContent="終わり"/>
